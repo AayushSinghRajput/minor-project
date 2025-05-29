10ac0789c4552a621e7a9c49c51c7875face7284
Aayush Kumar Singh <078bct003@ioepc.edu.np>
2025-05-29 12:51:59 +0545

    some changes

--- flask-service/final.py
+++ flask-service/final.py
@@ -6,6 +6,7 @@ from tensorflow.keras.models import load_model
 from sklearn.preprocessing import LabelEncoder
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -187,4 +188,5 @@ def predict_camera():
 
 # Run the Flask server
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    port =int(os.environ.get('PORT',8000))
+    app.run(host="0.0.0.0", port=port, debug=True)
